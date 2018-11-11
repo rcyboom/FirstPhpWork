f7acb0c354fab16d9eabbacb801f6e185ec83b70
rcyboom <rcyboom@gmail.com>
2018-11-11 21:01:31 +0800

    4

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -429,11 +429,10 @@ class TaskController extends Controller
      */
     public function TaskCars()
     {
-        $rs=DB::table('cartasks')
-            ->where('task_id','=',Request::input('task_id',0))
-            ->leftJoin('cars', 'cartasks.car_id', '=', 'cars.id')
-            ->select('cartasks.*','sum(rent_cost+oil_cost) as total', 'cars.car_type', 'cars.car_number','cars.phone')
-            ->get();
+        $rs=DB::select('select cartasks.*,cars.car_type,cars.car_number,cars.phone,'.
+            'sum(rent_cost+oil_cost+toll_cost+park_cost+award_salary) as total from cartasks '.
+            'left join cars on cartasks.car_id=cars.id where task_id=?',[Request::input('task_id',0)]);
+
         return $this->myResult(1,'信息获取成功！',$rs);
     }
 
@@ -450,11 +449,10 @@ class TaskController extends Controller
      */
     public function TaskMans()
     {
-        $rs=DB::table('usertasks')
-            ->where('task_id','=',Request::input('task_id',0))
-            ->leftJoin('users', 'usertasks.user_id', '=', 'users.id')
-            ->select('usertasks.*', 'users.name', 'users.phone_number')
-            ->get();
+         $rs=DB::select('select usertasks.*,users.name,users.phone_number,'.
+            'sum(work_salary+extra_salary+award_salary) as total from usertasks '.
+            'left join users on usertasks.user_id=users.id where task_id=?',[Request::input('task_id',0)]);
+
         return $this->myResult(1,'信息获取成功！',$rs);
     }
 
