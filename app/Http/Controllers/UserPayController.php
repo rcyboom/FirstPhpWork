747c54e21286d800329e5a287031fe7a03465b88
rcyboom <rcyboom@gmail.com>
2018-11-17 20:48:13 +0800

    3

--- app/Http/Controllers/UserPayController.php
+++ app/Http/Controllers/UserPayController.php
@@ -95,13 +95,13 @@ class UserPayController extends Controller
     public function saveOne($id=0)
     {
         $validator = Validator::make( Request::all(), [
-           // 'object_id' => 'required | integer | min:1',
-           // 'object_type' => 'required |in:人员,车辆',
+            'object_id' => 'required | integer | min:1',
+            'object_type' => 'required |in:人员,车辆',
             'time' => 'required | date',
-           // 'type' => 'required|in:奖励,惩罚,预支',
+            'type' => 'required|in:奖励,惩罚,预支',
             'reason' => 'required',
-            'money'=>'required | digits',
-            'score'=>'required | digits',
+            'money'=>'required | numeric',
+            'score'=>'required | numeric',
         ]);
 
         if ($validator->fails()) {
