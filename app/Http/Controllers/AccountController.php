e487c9213d9531051b150b35336cc6c7b53afd9a
rcyboom <rcyboom@gmail.com>
2019-01-06 17:32:01 +0800

    love

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -475,17 +475,11 @@ class AccountController extends Controller
         $usr=User::find(Request::input('user_id'));
         if($usr){
             $account_time=Request::input('account_time');
-            $user_task_id=Request::input('user_task_id',[]);
-            $user_pay_id=Request::input('user_pay_id',[]);
+            $user_task_id=implode(',',Request::input('user_task_id',[]));
+            $user_pay_id=implode(',',Request::input('user_pay_id',[]));
             $taskmoney =
-                DB::table('usertasks')
-                    ->where([
-                    ['account_id', '<', 1],
-                    ['user_id', '=', $usr->id],
-                        ])
-                    ->whereIn('id', $user_task_id)
-                    ->sum('work_salary+extra_salary+award_salary');
-
+                DB::select('select COALESCE(SUM(work_salary+extra_salary+award_salary),0) as cc from usertasks  where account_id<1 and user_id=? and id in (?)',
+                    [$usr->id,$user_task_id]);
             $usermoney =
                 DB::select('select COALESCE(SUM(money),0) as cc from userpays where account_id<1 and object_id=?  and object_type=? and id in (?)',
                     [$usr->id,'员工',$user_pay_id]);
