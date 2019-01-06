e8be05c20daf6f61e3e546558a9247196acc28b0
rcyboom <rcyboom@gmail.com>
2019-01-06 17:24:10 +0800

    love

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -479,13 +479,12 @@ class AccountController extends Controller
             $user_pay_id=Request::input('user_pay_id',[]);
             $taskmoney =
                 DB::table('usertasks')
-                    ->select('COALESCE(SUM(work_salary+extra_salary+award_salary),0) as cc')
                     ->where([
                     ['account_id', '<', 1],
                     ['user_id', '=', $usr->id],
                         ])
                     ->whereIn('id', $user_task_id)
-                    ->get();
+                    ->sum('work_salary+extra_salary+award_salary');
 
             $usermoney =
                 DB::select('select COALESCE(SUM(money),0) as cc from userpays where account_id<1 and object_id=?  and object_type=? and id in (?)',
